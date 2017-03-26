99617d5159568978542e52c9ecb5b7e07117c5fe
dylanwebb <dylanwebb@me.com>
2017-03-26 16:48:44 -0400

    Random Forest w/ Titles

--- titanic.r
+++ titanic.r
@@ -5,16 +5,22 @@ train <- read.csv(train_url)
 test_url <- "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
 test <- read.csv(test_url)
 
-# Load in the package
+# Load in the packages
 install.packages("randomForest")
 library(randomForest)
+install.packages("stringr")        
+library(stringr)
 
-# Train set and test set
-str(train)
-str(test)
+# Creat new variable: Title which is the passengers title
+Title <- str_match(train$Name, ".+, ([a-zA-Z\\.]+).+")
+train_2 <- train
+train_2$Title <- Title
+test_2 <- test_2
+test_2$Title < Title
 
 # Set seed for reproducibility
 set.seed(111)
+
 
 # Apply the Random Forest Algorithm
 my_forest <- randomForest(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title,
@@ -25,3 +31,6 @@ my_prediction <- predict(my_forest, test)
 
 # Create a data frame with two columns: PassengerId & Survived. Survived contains your predictions
 my_solution <- data.frame(PassengerId = test$PassengerId, Survived = my_prediction)
+
+# Write your solution away to a csv file with the name my_solution.csv
+write.csv(my_solution, file = "my_solution.csv", row.names = FALSE)
