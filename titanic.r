d70b36a55c64038a684bcdf9925925da6ad53fb0
dylanwebb <dylanwebb@me.com>
2017-03-27 14:12:39 -0400

    Added family size

--- titanic.r
+++ titanic.r
@@ -12,32 +12,36 @@ install.packages("stringr")
 library(stringr)
 library(rpart)
 
-# Creat new variable: Title which is the passengers title
+# Create new variable: Title which is the passengers title
 test$Survived <- NA
 all_data <- rbind(train, test)
-Title <- str_match(all_data$Name, ".+, ([a-zA-Z\\.]+).+")
-all_data$Title <- as.factor(Title[ ,2])
+Title <- str_match(all_data$Name, ".+, ([a-zA-Z]+).+")
+all_data$Title <- as.factor(Title[, 2])
 
+table(all_data$Sex, all_data$Title)
 
 # Fill in the NA values for all data
 all_data$Embarked[c(62, 830)] <- "S"
 all_data$Embarked <- factor(all_data$Embarked)
 all_data$Fare[1044] <- median(all_data$Fare, na.rm = TRUE)
 predicted_age <- rpart(Age ~ Pclass + Sex + SibSp + Parch + Fare + Embarked + Title
-                       , data = all_data[!is.na(all_data$Age),], method = "anova")
+                       , data = all_data[!is.na(all_data$Age), ], method = "anova")
 all_data$Age[is.na(all_data$Age)] <- predict(predicted_age
-                                             , all_data[is.na(all_data$Age),])
+                                             , all_data[is.na(all_data$Age), ])
+
+# Create a var for family size by adding siblings, spouses, parents, children, self
+all_data$famSize <- all_data$SibSp + all_data$Parch + 1
 
 # Split the data back into a train set and a test set
-train <- all_data[1:891,]
-test <- all_data[892:1309,]
+train <- all_data[1:891, ]
+test <- all_data[892:1309, ]
 test$Survived <- NULL
 
 # Set seed for reproducibility
 set.seed(111)
 
 # Apply the Random Forest Algorithm
-my_forest <- randomForest(as.factor(Survived) ~ Pclass + Sex + Age 
+my_forest <- randomForest(as.factor(Survived) ~ Pclass + Sex + Age + famSize
                           + SibSp + Parch + Fare + Embarked + Title,
                           data = train, importance = TRUE, ntree = 1000)
 
